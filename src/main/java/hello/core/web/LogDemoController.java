7c9270270882c09db382ba6273652f70fdea14f2
pok3mon025 <pok3mon025@gmail.com>
2021-07-09 17:54:18 +0900

    빈 스코프

---
+++ src/main/java/hello/core/web/LogDemoController.java
@@ -0,0 +1,35 @@
+package hello.core.web;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.beans.factory.ObjectProvider;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import hello.core.common.MyLogger;
+import lombok.RequiredArgsConstructor;
+
+@Controller
+@RequiredArgsConstructor
+public class LogDemoController {
+
+    private final LogDemoService logDemoService;
+    // private final ObjectProvider<MyLogger> myLoggerProvider;
+    private final MyLogger myLogger;
+
+    @RequestMapping("log-demo")
+    @ResponseBody
+    public String logDemo(HttpServletRequest request) throws InterruptedException {
+        String requestURL = request.getRequestURL().toString();
+        // MyLogger myLogger = myLoggerProvider.getObject();
+        System.out.println("myLogger = " + myLogger.getClass());
+        myLogger.setRequestURL(requestURL);
+
+        myLogger.log("controller test");
+        Thread.sleep(1000);
+        logDemoService.logic("testId");
+
+        return "OK";
+    }
+}
